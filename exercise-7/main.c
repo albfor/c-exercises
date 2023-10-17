a62e38eac1044a91ffcc053382c5f9447e246716
albfor <96afo02@gmail.com>
2023-10-17 15:36:41 +0200

    comments

--- exercise-7/main.c
+++ exercise-7/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/**
+* Definition of date struct
+*/
 typedef struct date_t {
     int day;
     int month;
@@ -10,7 +13,9 @@ int main(void)
 {
     date_t new_date;
     printf("Enter date (DD MM YYYY): ");
-    scanf("%d %d %d", &new_date.day, &new_date.month, &new_date.year);
+    // Read date data from user
+    scanf("%d %d %d", &new_date.day, &new_date.month, &new_date.year); 
+    // Print data using YYYY-MM-DD format
     printf("%04d-%02d-%02d\n", new_date.year, new_date.month, new_date.day);
 
     return 0;

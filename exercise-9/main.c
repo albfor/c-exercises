98896ba0953ac5448326ade80645c192993baf33
Albin Forsberg <96afo02@gmail.com>
2023-10-18 17:19:36 +0200

    working game

--- exercise-9/main.c
+++ exercise-9/main.c
@@ -3,28 +3,38 @@
 #include <time.h>
 
 #define MAX 100
+#define ATTEMPTS 10
 
 
 int main(void)
 {
-    int guess, res;
+    int guess;
     char term;
 
     srand(time(NULL));
     int secret_number = rand() % MAX;
-    printf("%d\n", secret_number);
+    printf("%d\n", secret_number); // TODO: remove this line before release :^)
     
-    printf("Enter an integer between 0 and %d: ", MAX);
-    while (!scanf("%d%c", &guess, &term)) {
-        while (getchar() != '\n'); // throw away the garbage input scanf failed to process.
-        printf("You are a dissapointment to your family.\n");
-        printf("Please, try entering an integer again...\n");
-    }
+    for (int i = 0; i < ATTEMPTS; i++) {
+        printf("Enter an integer between 0 and %d: ", MAX);
+        while (!scanf("%d%c", &guess, &term)) {
+            while (getchar() != '\n'); // throw away the garbage input scanf failed to process.
+            printf("You are a dissapointment to your family.\n");
+            printf("Please, try entering an integer again...\n");
+        }
 
-    if (guess == secret_number)
-        printf("Good boy!\n");
-    else 
-        printf("You tried.\n");
+        if (guess == secret_number) {
+            printf("Good boy!\n");
+            break;
+        }
+        else if (i == ATTEMPTS - 1)
+            printf("Get wrecked kiddo.\n");
+        else if (guess < secret_number)
+            printf("Too low.\n");
+        else 
+            printf("Too high.\n");
+
+    }
 
     return 0;
 }

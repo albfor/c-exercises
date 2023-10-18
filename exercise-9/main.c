485d5f66fabe2136048398c8897f2bd9103c43f1
Albin Forsberg <96afo02@gmail.com>
2023-10-18 17:29:35 +0200

    duger i krig

--- exercise-9/main.c
+++ exercise-9/main.c
@@ -9,31 +9,35 @@
 int main(void)
 {
     int guess;
-    char term;
+    char term, play_again = 'y';
 
-    srand(time(NULL));
-    int secret_number = rand() % MAX;
-    printf("%d\n", secret_number); // TODO: remove this line before release :^)
+    srand(time(NULL)); // seed random number generation
     
-    for (int i = 0; i < ATTEMPTS; i++) {
-        printf("Enter an integer between 0 and %d: ", MAX);
-        while (!scanf("%d%c", &guess, &term)) {
-            while (getchar() != '\n'); // throw away the garbage input scanf failed to process.
-            printf("You are a dissapointment to your family.\n");
-            printf("Please, try entering an integer again...\n");
+    while (play_again == 'y' || play_again == 'Y') {
+        int secret_number = rand() % MAX;
+        
+        for (int i = 0; i < ATTEMPTS; i++) {
+            printf("Enter an integer between 0 and %d: ", MAX);
+            while (!scanf("%d%c", &guess, &term)) {
+                while (getchar() != '\n'); // throw away the garbage input scanf failed to process.
+                printf("You are a dissapointment to your family.\n");
+                printf("Please, try entering an integer again...\n");
+            }
+
+            if (guess == secret_number) {
+                printf("Good boy!\n");
+                break;
+            }
+            else if (i == ATTEMPTS - 1)
+                printf("Get wrecked kiddo.\n");
+            else if (guess < secret_number)
+                printf("Too low.\n");
+            else 
+                printf("Too high.\n");
         }
 
-        if (guess == secret_number) {
-            printf("Good boy!\n");
-            break;
-        }
-        else if (i == ATTEMPTS - 1)
-            printf("Get wrecked kiddo.\n");
-        else if (guess < secret_number)
-            printf("Too low.\n");
-        else 
-            printf("Too high.\n");
-
+        printf("Play again? (y/n)");
+        scanf("%c", &play_again);
     }
 
     return 0;

a0e484597020d1ff652cfded5df7d79ef395c1cd
albfor <96afo02@gmail.com>
2023-11-01 10:57:11 +0100

    easier way to read a string

--- week44/exercise-19/main.c
+++ week44/exercise-19/main.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 
+#define LENGTH 100
+
 int main(void)
 {
-    char *line;
-    size_t size;
-
+    char line[LENGTH]; 
+    
     printf("The input string: ");
-    getline(&line, &size, stdin);
+    fgets(line, LENGTH, stdin);
     printf("The output string:\n");
     int len = strlen(line);             // length of input
     for (int i = 0; i < len; i++) {

69b9efbf3fbd94db4fbdeb23bd44735df262362e
Albin Forsberg <96afo02@gmail.com>
2023-11-02 11:01:18 +0100

    clean up

--- week44/exercise-17/main.c
+++ week44/exercise-17/main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
+#define LENGTH 255
+
 int main(void)
 {
     // init
-    char string[255];           // arbitrarily selected string size
-    scanf("%s", string);        // read string
-    int len = strlen(string);   // get length of string
+    char string[LENGTH];                    // arbitrarily selected string size
+    fgets(string, sizeof(string), stdin);   // read string
+    int len = strlen(string);               // get length of string
 
     // print the string in reverse order
     for (int i = 0; i < len; i++) {
-        printf("%c", string[len - (1 + i)]);
+        putchar(string[len - (1 + i)]);
     }
     printf("\n");
 

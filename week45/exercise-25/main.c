4eebdf5092f625ac099e5caf3d6b5944acbaf9a1
Albin Forsberg <96afo02@gmail.com>
2023-11-09 10:45:30 +0100

    comments

--- week45/exercise-25/main.c
+++ week45/exercise-25/main.c
@@ -3,9 +3,13 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define MIN 1
+#define MAX 99
+
 void fill_arr(int *arr, int len);
 void print_arr(int *arr, int len);
 bool find(int value, int *arr, int len);
+int get_rand_range(int start, int end);
 
 int main(void)
 {
@@ -19,19 +23,38 @@ int main(void)
     return 0;
 }
 
+/**
+ * fill @arr with unique random integers in the range 
+ */
 void fill_arr(int *arr, int len)
 {
     for (int i = 0; i < len; i++) {
-        while(find(arr[i] = (rand() % 99) + 1, arr, i)); 
+        // keep reading random numbers until we find somthing that doesn't 
+        // already exist in the array
+        while(find(arr[i] = get_rand_range(MIN, MAX), arr, i)); 
     }
 }
 
+/**
+ * @return a random integer in the inclusive range @start to @end.
+ */
+int get_rand_range(int start, int end)
+{
+    return rand() % (end + 1 - start) + start;
+}
+
+/**
+ * @value to search for
+ * @arr array to search
+ * @len length of array at @arr 
+ * @return true if @value is found in @arr
+ */
 bool find(int value, int *arr, int len)
 {
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len; i++)
         if (arr[i] == value)
             return true;
-    }
+
     return false;
 }
 
@@ -43,3 +66,4 @@ void print_arr(int *arr, int len)
     }
     printf("%d }\n", arr[len - 1]);
 }
+
